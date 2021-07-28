f10b55921de6dff19401ee6304035e8d25aa4ac2
pflis <piotrek610@wp.pl>
2021-07-28 16:26:58 +0200

    zadanie36

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let liczba1 = 10;
+let liczba2 = 10;
+if (liczba1 == 20) {
+    console.log('true');
+} else if (liczba2 == 20) {
+    console.log('true');
+} else if (liczba1+liczba2 <= 20) {
+    console.log('true');
+} else {
+    console.log('false');
+}
