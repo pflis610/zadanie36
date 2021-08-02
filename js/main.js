69c5c2244842b4de9105284a4382783615678118
pflis610 <piotrek610@wp.pl>
2021-08-02 14:44:24 +0200

    zadanie36 dopieszczenie

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 let liczba1 = 10;
 let liczba2 = 10;
-let numbercheck = function (liczba1, liczba2) {
-if (liczba1 == 20) {
+let numbercheck = function (li1, li2) {
+    if (li1 == 20) {
     return true;
-} else if (liczba2 == 20) {
-    return true;
-} else if (liczba1+liczba2 <= 20) {
-    return true;
-} else {
-    return false;
-    }
+    } else if (li2 == 20) {
+      return true;
+    } else if (li1+li2 <= 20) {
+      return true;
+    } else {
+      return false;
+      }
 }
 console.log(numbercheck(liczba1, liczba2));
